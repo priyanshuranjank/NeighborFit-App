de60d4d4edfd0d32df4f9898efa39b7312311b77
priyanshuranjank <priyanshuranjan1r@gmail.com>
2025-07-09 01:10:39 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,7 @@
 require('dotenv').config();
 const app = require('./app');
 
-const PORT = process.env.PORT;
-
-app.get('/', (req, res) => {
-  res.send({
-    activeStatus: true,
-    error: false
-  });
-});
-
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`backend running on port ${PORT}`);
 });
