c24261b027b6ee0c4d1382299626a0857e80b05b
priyanshuranjank <priyanshuranjan1r@gmail.com>
2025-07-09 01:03:28 +0530

    added vercel.json

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,14 @@ require('dotenv').config();
 const app = require('./app');
 
 const PORT = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send({
+    activeStatus: true,
+    error: false
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`backend running on port ${PORT}`);
 });
